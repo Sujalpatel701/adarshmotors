544c54f33afb578f0df597b908f64824a3cafde9
sujal patel <worldonwheels701@gmail.com>
2023-09-28 10:14:16 -0500

    first commit on admin change

--- admin/admindashboard.php
+++ admin/admindashboard.php
@@ -3,57 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Dashboard</title>
-    
-    <!-- Add Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="../css/admnstyle.css">
+    <link rel="stylesheet" href="../css/all.min.css">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@500&display=swap" rel="stylesheet">
 </head>
 <body>
-
-<!-- Navigation Bar -->
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">Admin Dashboard</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-                <a class="nav-link" href="#">Dashboard</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Users</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Settings</a>
-            </li>
-        </ul>
-    </div>
-</nav>
-
-<!-- Main Content Area -->
-<div class="container mt-4">
-    <div class="row">
-        <div class="col-lg-3">
-            <!-- Sidebar -->
-            <div class="list-group">
-                <a href="#" class="list-group-item list-group-item-action">Dashboard</a>
-                <a href="#" class="list-group-item list-group-item-action">Users</a>
-                <a href="#" class="list-group-item list-group-item-action">Products</a>
-                <a href="#" class="list-group-item list-group-item-action">Settings</a>
-            </div>
-        </div>
-        <div class="col-lg-9">
-            <!-- Content -->
-            <h1>Welcome to the Admin Dashboard</h1>
-            <p>This is a simple Bootstrap admin dashboard.</p>
-        </div>
-    </div>
-</div>
-
-<!-- Add Bootstrap JS and jQuery -->
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <nav class="navbar navbar-dark fixed-top" style="background-color:  #0e1f0b; padding: 10px;color: white;">
+<a href="admindashboard.php" style="font-size:40px;color: white;font-family: 'Ubuntu', sans-serif;">ADARSH <small>MOTORS</small></a></nav>
 </body>
 </html>
